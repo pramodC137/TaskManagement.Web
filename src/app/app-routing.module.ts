b3cd29cddef5884ca7e1b24faf6831d90dde0016
pramodC135 <pramodmadhushan4@gmail.com>
2026-01-13 10:09:42 +0530

    added api url to ent and change routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { AuthComponent } from './components/auth/auth.component'; // Import the 
 import { TaskManagerComponent } from './components/task-manager/task-manager.component';
 
 const routes: Routes = [
-  { path: 'auth', component: AuthComponent }, // Add route for AuthComponent
-  { path: '', redirectTo: 'auth', pathMatch: 'full' }, // Optional: redirect root to auth
-  { path: 'task-manager', component: TaskManagerComponent }, 
+  //{ path: 'auth', component: AuthComponent }, // Add route for AuthComponent
+  //{ path: '', redirectTo: 'auth', pathMatch: 'full' }, // Optional: redirect root to auth
+  //{ path: 'task-manager', component: TaskManagerComponent }, 
 ];
 
 @NgModule({
